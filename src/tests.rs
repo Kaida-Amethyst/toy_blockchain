13a72ec383702f1fdfa34eb27c48420b6c5d0cde
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2024-06-06 21:42:50 +0800

    add one test

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
     use crate::block::Block;
+    use crate::blockchain::{BlockChain, BLOCKS_TREE_NAME, TIP_BLOCK_HASH_KEY};
     use crate::transaction::Transaction;
 
     #[test]
@@ -15,5 +16,18 @@ mod tests {
         let genesis_pre_hash = String::from("0x0");
         let bk = Block::new(genesis_pre_hash, vec![tx], 0);
         bk.print();
+    }
+
+    #[test]
+    fn create_blockchain() {
+        let blockchain = BlockChain::create_blockchain("abxgtsunkodojahucd");
+        println!("Tip block hash: {}", blockchain.get_tip_hash());
+        let db: &sled::Db = blockchain.get_db();
+        let blocks_tree = db.open_tree(BLOCKS_TREE_NAME).unwrap();
+        let tip_block_hash = blocks_tree.get(TIP_BLOCK_HASH_KEY).unwrap();
+        let tip_block_hash = String::from_utf8(tip_block_hash.unwrap().to_vec()).unwrap();
+        let tip_block_data = blocks_tree.get(tip_block_hash).unwrap();
+        let tip_block = Block::deserialize(&tip_block_data.unwrap());
+        tip_block.print();
     }
 }
